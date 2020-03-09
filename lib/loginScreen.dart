7d0580b62c833d960df4ee7228a9fc979d9c2a49
mahmoud_elebiary <mahmoud.elebiary@yahoo.com>
2020-03-09 17:14:28 +0200

    create login screen

---
+++ lib/loginScreen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.only(
+            left: 0.0, top: 25.0, right: 0.0, bottom: 0.0),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  "Login",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.blue),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Enter your username'),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Enter yout password"),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: <Widget>[
+                    Text("no Account yet?"),
+                    Container(
+                      margin: const EdgeInsets.only(
+                          left: 5.0, top: 0.0, right: 5.0, bottom: 0.0),
+                      child: InkWell(
+                        child: Text(
+                          "SignIn",
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                        onTap: () {
+                          Toast.show("yaaaaaaaaaah", context,
+                              duration: Toast.LENGTH_SHORT,
+                              gravity: Toast.BOTTOM);
+                        },
+                      ),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: RaisedButton(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                    color: Colors.amber,
+                    onPressed: () {
+                      onClickBtn();
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onClickBtn() {
+    Toast.show("Toast plugin app", context,
+        duration: Toast.LENGTH_SHORT, gravity: Toast.BOTTOM);
+  }
+}
